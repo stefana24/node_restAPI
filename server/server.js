0bdb66093fdab83d039b3177d4ce647311d8cc5f
stefana24 <stefi_campean@yahoo.com>
2023-05-13 17:56:22 +0300

    register user

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 import fetch from "node-fetch";
 import express from "express";
 // import TOKEN from "../token.js";
-const TOKEN =
-  "845de8800d3d43db3b801019e45ed305b5c49183d14174d0c0c6da0e966bf2b1";
-import { checkSchema } from "express-validator";
+const TOKEN = process.env.TOKEN;
+import { checkSchema, validationResult } from "express-validator";
 import axios from "axios";
 
 const app = express();
@@ -23,31 +22,24 @@ const __dirname = path.dirname(__filename);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/users", async (req, res) => {
-  const response = await fetch(
-    "https://gorest.co.in/public/v2/users?page=1&per_page=20"
-  );
-  const users = await response.json();
-  res.send({ users: users });
-});
-
-app.get("/add_user", (req, res) => {
-  res.sendFile(__dirname + "/newUser.html");
-});
-
 app.post(
-  "/add_user",
+  "/api/register",
   checkSchema({
-    email: { isEmail: true, notEmpty: true },
+    email: {
+      isEmail: {
+        errorMessage: "Must be a valid e-mail address",
+      },
+    },
     name: { notEmpty: true },
     gender: { notEmpty: true },
     status: { notEmpty: true },
   }),
   async (req, res) => {
     try {
-      // Make a POST request to the API
-      const response = await axios.post(
-        "https://gorest.co.in/public/v2/users",
+      const errors = validationResult(req);
+      const errorMessages = errors.array();
+      const registerResponse = await axios.post(
+        "https://gorest.co.in/public-api/users",
         req.body,
         {
           headers: {
@@ -57,47 +49,21 @@ app.post(
         }
       );
 
-      // Handle the response from the API
-      console.log(response.data); // Do something with the response
+      const registeredUser = registerResponse.data.data;
+      console.log(registeredUser); 
 
-      // Send a response back to the client
-      res
-        .status(200)
-        .json({ success: true, message: "POST request sent successfully" });
+      res.status(200).json({
+        success: true,
+        message: "User registered successfully",
+        body: req.body,
+        errors: errorMessages,
+      });
     } catch (error) {
       console.error(error);
       res.status(500).json({ success: false, message: "An error occurred" });
     }
   }
 );
-
-app.post("/api/register", async (req, res) => {
-  try {
-    // Make a POST request to register a user
-    const registerResponse = await axios.post(
-      "https://gorest.co.in/public-api/users",
-      req.body,
-      {
-        headers: {
-          Authorization: "Bearer " + TOKEN,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-
-    const registeredUser = registerResponse.data.data;
-    console.log(registeredUser); // Display the registered user
-
-    res.status(200).json({
-      success: true,
-      message: "User registered successfully",
-      user: registeredUser,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, message: "An error occurred" });
-  }
-});
 
 // Endpoint to handle the GET request to fetch all users
 app.get("/api/users", async (req, res) => {
--- token.js
+++
@@ -1,3 +0,0 @@
-const TOKEN =
-  "845de8800d3d43db3b801019e45ed305b5c49183d14174d0c0c6da0e966bf2b1";
-export default TOKEN;
